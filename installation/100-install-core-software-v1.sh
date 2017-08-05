0b234a4740b63796163656784e78cbd6171af6a3
Adam Vitali <adamvnz@techie.com>
2017-08-05 19:44:43 +1200

    Update 100-install-core-software-v1.sh

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -12,18 +12,18 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
-sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
-sudo pacman -S --noconfirm --needed dmidecode 
+sudo pacman -S --noconfirm --needed archey3 bleachbit catfish clementine
+# sudo pacman -S --noconfirm --needed dmidecode 
 sudo pacman -S --noconfirm --needed evince filezilla firefox
 sudo pacman -S --noconfirm --needed gimp git gksu gnome-disk-utility 
-sudo pacman -S --noconfirm --needed gparted gpick grsync
+sudo pacman -S --noconfirm --needed gparted grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp htop 
 sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors lsb-release meld mlocate
-sudo pacman -S --noconfirm --needed net-tools numlockx openshot pinta plank
-sudo pacman -S --noconfirm --needed redshift sane screenfetch
-sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder smplayer sysstat 
+sudo pacman -S --noconfirm --needed net-tools numlockx pinta plank
+sudo pacman -S --noconfirm --needed sane screenfetch
+sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder sysstat 
 sudo pacman -S --noconfirm --needed thunderbird
-sudo pacman -S --noconfirm --needed variety vlc vnstat wget
+sudo pacman -S --noconfirm --needed vlc vnstat wget
 
 
 sudo systemctl enable vnstat
