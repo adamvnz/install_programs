888a0643ee823d592968fbc345d3cc68c3ae41e1
Adam Vitali <adamvnz@techie.com>
2017-08-26 11:24:34 +1200

    Update 100-install-core-software-v1.sh

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -13,17 +13,17 @@ set -e
 
 #software from 'normal' repositories
 sudo pacman -S --noconfirm --needed archey3 bleachbit catfish clementine
-# sudo pacman -S --noconfirm --needed dmidecode 
+sudo pacman -S --noconfirm --needed dmidecode 
 sudo pacman -S --noconfirm --needed evince filezilla firefox
-sudo pacman -S --noconfirm --needed gimp git gksu gnome-disk-utility 
+sudo pacman -S --noconfirm --needed git gksu gnome-disk-utility 
 sudo pacman -S --noconfirm --needed gparted grsync
 sudo pacman -S --noconfirm --needed hardinfo hddtemp htop 
 sudo pacman -S --noconfirm --needed inkscape inxi lm_sensors lsb-release meld mlocate
-sudo pacman -S --noconfirm --needed net-tools numlockx openssh pinta plank
+sudo pacman -S --noconfirm --needed net-tools numlockx openssh openvpn networkmanager-openvpn pinta
 sudo pacman -S --noconfirm --needed sane screenfetch
-sudo pacman -S --noconfirm --needed simple-scan simplescreenrecorder sysstat 
+sudo pacman -S --noconfirm --needed simplescreenrecorder sysstat 
 sudo pacman -S --noconfirm --needed thunderbird
-sudo pacman -S --noconfirm --needed vlc vnstat wget
+sudo pacman -S --noconfirm --needed vlc vnstat xsane wget
 
 
 sudo systemctl enable vnstat
